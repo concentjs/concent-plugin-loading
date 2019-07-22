54f215253c5267d100c844732fb609b537781523
fancyzhong <fancyzhong@tencent.com>
2019-07-22 12:08:47 +0800

    按最新的插件定义,暴露install函数

--- index.js
+++ index.js
@@ -76,59 +76,6 @@ function _makeFnLoadingState(reducerMod) {
   return state;
 }
 
-/** concent启动时会调用一次, 用户configure */
-toExport.getConf = function () {
-  var state = {};
-  if (fnLoading) {
-    state = _makeFnLoadingState();
-  } else {
-    var moduleName_stateKeys_ = ccContext.moduleName_stateKeys_;
-    Object.keys(moduleName_stateKeys_).forEach(function (mod) {
-      state[mod] = false;
-    });
-  }
-
-  return {
-    name: pluginName,
-    module: pluginName,
-    conf: { state: state },//用于给configure配置用
-    extra: {
-      module_trueLoadingCount: module_trueLoadingCount,
-      moduleAndFnName_isAsyncFn_: moduleAndFnName_isAsyncFn_,
-      fnLoading: fnLoading,
-      onlyForAsync: onlyForAsync,
-    }
-  }
-}
-
-/**
- * @param {{fnLoading:boolean, onlyForAsync:boolean}} conf fnLoading default is true, onlyForAsync default is false
- */
-toExport.setConf = function (conf) {
-  if (conf) {
-    var _fnLoading = conf.fnLoading;
-    var _onlyForAsync = conf.onlyForAsync;
-    var _enqueue = conf.enqueue;
-    if (_fnLoading !== undefined) fnLoading = _fnLoading;
-    if (_onlyForAsync !== undefined) onlyForAsync = _onlyForAsync;
-    if (_enqueue !== undefined) enqueue = _enqueue;
-  }
-}
-
-/** 如定义此函数，concent掉用configure配置模块时，会调用此逻辑 */
-toExport.writeModuleState = function (pluginModuleState, newModule) {
-  var toSet = {};
-  if (fnLoading) {
-    state = _makeFnLoadingState(newModule);
-    appendState(pluginName, toSet);
-    return;
-  }
-
-  toSet[newModule] = false;
-  appendState(pluginName, toSet);
-  module_trueLoadingCount[newModule] = 0;
-}
-
 var latestLoading = true;
 var enqueuedState = {};
 var timer = 0;
@@ -163,7 +110,7 @@ function _enqueueLoadingStatus(fnKey, loading) {
   }, 190);
 }
 
-function _makeFnLoadingStateToSet(module, fnName, loading, toSetObj){
+function _makeFnLoadingStateToSet(module, fnName, loading, toSetObj) {
   var key = module + '/' + fnName;
   var moduleKey = module + '/*';
   var toSet = toSetObj || {};
@@ -185,7 +132,7 @@ function _makeFnLoadingStateToSet(module, fnName, loading, toSetObj){
   if (newCount > 0 && pluginState[moduleKey] !== true) {
     toSet[moduleKey] = true;
   }
-  if(newCount === 0 && pluginState[moduleKey] === true){
+  if (newCount === 0 && pluginState[moduleKey] === true) {
     toSet[moduleKey] = false;
   }
 
@@ -202,11 +149,11 @@ function setFnLoadingStatus(module, fnName, loading) {
 
   var pluginState = getState(pluginName);
   var prevLoadingStatus = pluginState[key];
-  if(loading === true){
-    if(prevLoadingStatus !== true) {
+  if (loading === true) {
+    if (prevLoadingStatus !== true) {
       _enqueueLoadingStatus(key, true);
     };
-  }else{
+  } else {
     //不检查prevLoadingStatus（来自于store的值很可能有还未变化，
     //因为如果时间够短，可能enqueuedState里有一批为true的loading还未提交），
     //直接触发_enqueueLoadingStatus
@@ -254,36 +201,85 @@ function setLoadingFalse(module, fnName) {
   }
 }
 
-toExport.receive = function (sig, payload) {
-  var fn = payload.fn;
-  if (!fn) return;//有可能非reducer调用
-  if (payload.calledBy == 'invoke') return;//invoke调用，无loading特效
-  if (!payload.isSourceCall) return;//非源头触发的调用，无loading特效
-
-  var module = payload.module;
-
-  var fnName = fn.__fnName || fn.name;
-  if (cst.SIG_FN_START === sig) {
-    if (onlyForAsync === true) {//处于只有async函数才需要有loading的工作模式
-      if (isAsyncFunction(module, fn)) {
-        setLoadingTrue(module, fnName);
-      }
-      return;
-    }
-    setLoadingTrue(module, fnName);
-    return;
+/** concent启动时会调用一次插件的install接口 */
+toExport.install = function (on) {
+  var state = {};
+  if (fnLoading) {
+    state = _makeFnLoadingState();
+  } else {
+    var moduleName_stateKeys_ = ccContext.moduleName_stateKeys_;
+    Object.keys(moduleName_stateKeys_).forEach(function (mod) {
+      state[mod] = false;
+    });
   }
 
-  if ([cst.SIG_FN_END, cst.SIG_FN_ERR, cst.SIG_FN_QUIT].indexOf(sig) !== -1) {
-    if (onlyForAsync === true) {
-      if (isAsyncFunction(module, fn)) {
-        setLoadingFalse(module, fnName);
+  // 将loading配置成concent的模块
+  configure(pluginName, { state });
+
+  on([cst.SIG_FN_START, cst.SIG_FN_END, cst.SIG_FN_ERR, cst.SIG_FN_QUIT], function(data){
+    var payload = data.payload;
+    var sig = data.sig;
+
+    var fn = payload.fn;
+    if (!fn) return;//有可能非reducer调用
+    if (payload.calledBy == 'invoke') return;//invoke调用，无loading特效
+    if (!payload.isSourceCall) return;//非源头触发的调用，无loading特效
+  
+    var module = payload.module;
+  
+    var fnName = fn.__fnName || fn.name;
+    if (cst.SIG_FN_START === sig) {
+      if (onlyForAsync === true) {//处于只有async函数才需要有loading的工作模式
+        if (isAsyncFunction(module, fn)) {
+          setLoadingTrue(module, fnName);
+        }
+        return;
       }
+      setLoadingTrue(module, fnName);
       return;
     }
-    setLoadingFalse(module, fnName);
-  }
+  
+    if ([cst.SIG_FN_END, cst.SIG_FN_ERR, cst.SIG_FN_QUIT].indexOf(sig) !== -1) {
+      if (onlyForAsync === true) {
+        if (isAsyncFunction(module, fn)) {
+          setLoadingFalse(module, fnName);
+        }
+        return;
+      }
+      setLoadingFalse(module, fnName);
+    }
+  });
+
+  on(cst.SIG_MODULE_CONFIGURED, function (data) {
+    var newModule = data.payload;
+
+    var toSet = {};
+    if (fnLoading) {
+      toSet = _makeFnLoadingState(newModule);
+      appendState(pluginName, toSet);
+      return;
+    }
+
+    toSet[newModule] = false;
+    appendState(pluginName, toSet);
+    module_trueLoadingCount[newModule] = 0;
+  })
+
+  return { name: pluginName }
 }
 
+/**
+ * @param {{fnLoading:boolean, onlyForAsync:boolean}} conf fnLoading default is true, onlyForAsync default is false
+ */
+toExport.setConf = function (conf) {
+  if (conf) {
+    var _fnLoading = conf.fnLoading;
+    var _onlyForAsync = conf.onlyForAsync;
+    var _enqueue = conf.enqueue;
+    if (_fnLoading !== undefined) fnLoading = _fnLoading;
+    if (_onlyForAsync !== undefined) onlyForAsync = _onlyForAsync;
+    if (_enqueue !== undefined) enqueue = _enqueue;
+  }
+}
 
 toExport.default = toExport;
